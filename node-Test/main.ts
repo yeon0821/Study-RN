2a3f13415bb985fac40c1f34fd2c767c880b154f
yeonu <yeon8747@naver.com>
2023-10-18 23:04:13 +0900

    node Test

---
+++ node-Test/main.ts
@@ -0,0 +1,15 @@
+import _ from 'lodash'
+
+console.log(_.upperCase('hello-world'))
+
+interface User {
+    name: String
+    age: Number
+}
+
+const user: User = {
+    name: 'tlqkf',
+    age: 18
+}
+
+console.log(user)

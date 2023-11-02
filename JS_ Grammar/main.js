3561ae52b3d1ff8417f612d2660835cee191cd02
yeonu <yeon8747@naver.com>
2023-11-02 19:54:37 +0900

    JS :: String, Number

---
+++ JS_ Grammar/main.js
@@ -0,0 +1,20 @@
+// String
+const string1 = "Hello"
+const string2 = 'Hello'
+const string3 = `Hello ${string1} ?!`
+
+console.log(string3)
+
+//Number
+const Number = 123
+const pi = .14
+
+console.log(typeof(Number + undefined))
+console.log(typeof(pi))
+
+const a = 0.1
+const b = 0.2
+
+console.log(((a + b).toFixed(1))) // 0,3
+
+

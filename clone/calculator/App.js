f4a2e2c2b3d89df3554cbf804aee89606db5708c
yeonu <yeon8747@naver.com>
2024-02-02 03:20:35 +0900

    clone:: Calculator

---
+++ clone/calculator/App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
+import { SafeAreaView, StyleSheet } from 'react-native';
+import Calculator from './src/Calculator';
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+     <Calculator/>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ clone/calculator/src/Calculator.js
@@ -0,0 +1,173 @@
+import React from 'react';
+import { Text, TouchableOpacity, View } from 'react-native';
+import styled from 'styled-components/native';
+import { useCalculator } from './use-Calculator';
+
+const COLOR = {
+  RESULT: '#4e4c51',
+  RESET: '#5f5e62',
+  OPERATOR: '#f39c29',
+  NUM: '#5c5674',
+}
+
+const oneBlockWidth = 80; // 한블럭에 해당하는 가로길이
+
+// Button type: 'reset' | 'operator' | 'num'
+const Button = ({ text, onPress, flex, type, isSelected }) => {
+  const backgroundColor = 
+    type === 'reset' 
+      ? COLOR.RESET 
+      : type === 'operator' 
+        ? COLOR.OPERATOR 
+        : type === 'num' 
+          ? COLOR.NUM 
+          : 'transparent';
+  return (
+    <TouchableOpacity 
+      onPress={onPress} 
+      style={{ 
+        backgroundColor,
+        justifyContent: "center",
+        alignItems: "center",
+        height: 50,
+        width: oneBlockWidth * flex,
+        borderWidth: isSelected ? 1 : 0.2,
+        borderColor: "black",
+      }}>
+      <Text style={{ color: "white", fontSize: 25 }}>{text}</Text>
+    </TouchableOpacity>
+  )
+}
+
+const ButtonContainer = styled.View`
+  flex-direction: row;
+  width: 100%;
+`;
+const InputContainer = styled.View
+
+
+export default () => {
+  const {
+    input,
+    currentOperator,
+    result,
+    tempInput,
+    tempOperator,
+    hasInput,
+    onPressNum,
+    onPressOperator,
+    onPressReset,
+  } = useCalculator();
+
+  return (
+    <View style={{ flex: 1, justifyContent: "center" }}>
+      {__DEV__ && (
+        <>
+          <Text>input: {input}</Text>
+          <Text>currentOperator: {currentOperator}</Text>
+          <Text>result: {result}</Text>
+          <Text>tempInput: {tempInput}</Text>
+          <Text>tempOperator: {tempOperator}</Text>
+        </>
+      )}
+
+      {/* 결과 */}
+      {/* <InputContainer>
+        <Text style={{ color: "white", fontSize: 35, textAlign: "right" }}>{input}</Text>
+      </InputContainer> */}
+
+      {/* [AC ~ /] */}
+      <ButtonContainer>
+        <Button
+          type="reset"
+          text={hasInput ? "C" : "AC"}
+          onPress={onPressReset}
+          flex={3}
+        />
+        <Button
+          type="operator"
+          text="/"
+          onPress={() => onPressOperator("/")}
+          flex={1}
+          isSelected={currentOperator === "/"}
+        />
+      </ButtonContainer>
+
+      {/* [7 ~ x] */}
+      <ButtonContainer>
+        {[7, 8, 9].map((num) => (
+          <Button
+            key={`num-${num}`}
+            type="num"
+            text={`${num}`}
+            onPress={() => onPressNum(num)}
+            flex={1}
+          />
+        ))}
+        <Button
+          type="operator"
+          text="*"
+          onPress={() => onPressOperator("*")}
+          flex={1}
+          isSelected={currentOperator === "*"}
+        />
+      </ButtonContainer>
+
+      {/* [4 ~ -] */}
+      <ButtonContainer>
+        {[4, 5, 6].map((num) => (
+          <Button
+            key={`num-${num}`}
+            type="num"
+            text={`${num}`}
+            onPress={() => onPressNum(num)}
+            flex={1}
+          />
+        ))}
+        <Button
+          type="operator"
+          text="-"
+          onPress={() => onPressOperator("-")}
+          flex={1}
+          isSelected={currentOperator === "-"}
+        />
+      </ButtonContainer>
+
+      {/* [1 ~ +] */}
+      <ButtonContainer>
+        {[1, 2, 3].map((num) => (
+          <Button
+            key={`num-${num}`}
+            type="num"
+            text={`${num}`}
+            onPress={() => onPressNum(num)}
+            flex={1}
+          />
+        ))}
+        <Button
+          type="operator"
+          text="+"
+          onPress={() => onPressOperator("+")}
+          flex={1}
+          isSelected={currentOperator === "+"}
+        />
+      </ButtonContainer>
+
+      {/* [0 ~ =] */}
+      <ButtonContainer>
+        <Button
+          type="num"
+          text="0"
+          onPress={() => onPressNum(0)}
+          flex={3}
+        />
+        <Button
+          type="operator"
+          text="="
+          onPress={() => onPressOperator("=")}
+          flex={1}
+        />
+      </ButtonContainer>
+    </View>
+  )
+}
